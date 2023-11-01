7faba7dfa26fec884c58e730cf8083838b2fb95f
ShulcN <95365569+ShulcN@users.noreply.github.com>
2023-11-01 10:50:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-# new file
-print('Hello wrold!')
-print('hihihi-ha')
-print("new version 4")
+from ota import OTAUpdater
+import time
+# new
+firmware_url = "https://raw.githubusercontent.com/ShulcN/test_pi_ota/"
+ota_updater = OTAUpdater("POCO M4 Pro", "123456789f", firmware_url, "main.py")
+#def update_2(timer):
+   # global ota_updater
+   # ota_updater.download_and_install_update_if_available()
+   # ota_updater.download_and_install_update_if_available()
+#ota_updater.download_and_install_update_if_available()
+while True:
+    ota_updater.download_and_install_update_if_available()
+    time.sleep(1)
+#timer.init(freq=0.2, mode=Timer.PERIODIC, callback=update_2)
